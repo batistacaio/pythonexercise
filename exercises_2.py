0a445c2faef567903361e6a4212a73d1ecf5385d
caio_b_s@hotmail.com <batistacaio>
2022-04-06 13:15:45 -0300

    Starting with loops

--- exercises_2.py
+++ exercises_2.py
@@ -1,4 +1,5 @@
 #Booleans
+
 class myclass():
     def __len__(self):
         return 0
@@ -26,3 +27,45 @@ z = 4
 print(y ** z) #Exponentiation
 print(y // z) #Floor division
 
+#Lists
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+print(len(mylist))
+#Constructor method
+thislist = list(("apple", 1, False, "banana"))
+print(thislist)
+
+#-----
+#Four types of collections (arrays) => 
+#1 - list (ordered and changeable - allow duplicates), 
+#2 - tuple (ordered and unchangeable - allow duplicates), 
+#3 - set (unordered and unchangeable, but can add/remove items - do not allow duplicates) and 
+#4 - dictionary (ordered and changeable - do not allow duplicates)
+#-----
+
+#Check if
+
+if "apple" in thislist:
+    print("Found some apples")
+    
+#Change values
+    
+thislist[2] = True
+print(thislist)
+
+#Insert items
+
+thislist.insert(4, 43.2)
+print(thislist)
+#-Use append to add to the end of the list
+
+#Extend list
+
+thislist.extend(mylist)
+print(thislist)
+
+#Remove last item
+
+thislist.pop()
+print(thislist)
