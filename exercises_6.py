836ed27e3c46a9b3b7fae392157170b5c634e628
caio_b_s@hotmail.com <batistacaio>
2022-04-08 15:31:52 -0300

    Importing first module

--- exercises_6.py
+++ exercises_6.py
@@ -1,5 +1,8 @@
-import mymodule
+from mymodule import person1
 
 #Modules
 
-mymodule.greeting("Caio")
+def greeting(at1, at2, at3):
+    print("Hello, my name is " + at1 + ", i'm " + at2 + " years old and i'm a " + at3)
+    
+greeting(person1["name"], str(person1["age"]), person1["profession"])
