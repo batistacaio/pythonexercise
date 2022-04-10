df4b574a3dc8b87b23b4c5b62040887ce737d37d
CaioB99 <caio_b_s@hotmail.com>
2022-04-09 21:37:59 -0300

    Update challenge

--- challenge_1.py
+++ challenge_1.py
@@ -7,3 +7,10 @@ ano_nascimento = ano_atual - idade
 imc = peso / altura ** 2
 
 print(f"Meu nome é {nome}, tenho {idade} anos, nasci no ano de {ano_nascimento} e meu IMC é {imc:.2f}")
+
+if (idade < 16):
+    print(f"{nome} não pode votar")
+elif (idade >= 16 and idade < 18):
+    print(f"{nome} pode votar se quiser")
+else:
+    print(f"{nome} deverá votar obrigatoriamente")
