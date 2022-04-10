9773af3f5a2bfe37471e4a1f7f9779e1fb2fecf5
CaioB99 <caio_b_s@hotmail.com>
2022-04-10 18:45:30 -0300

    Update challenge

--- challenge_2.py
+++ challenge_2.py
@@ -1,3 +1,4 @@
+"""
 usuario = input("Nome de usuário: ")
 senha = input("Senha do usuário: ")
 
@@ -7,3 +8,14 @@ elif len(senha) < 8:
     print("Senha deverá ter ao menos 8 dígitos.")
 else:
     print("Usuário cadastrado.")
+"""
+
+num1 = input("Digite um número: ")
+num2 = input("Digite outro número: ")
+
+if num1.isnumeric() and num2.isnumeric():
+    num1 = int(num1)
+    num2 = int(num2)
+    print(num1 + num2)
+else:
+    print("Digite apenas números inteiros positivos.")
