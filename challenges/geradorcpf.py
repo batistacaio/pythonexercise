03b3d14f7e60be55c22dcca00a895926f5bee57a
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-04-14 13:08:31 -0300

    Geradorcpf

--- challenges/geradorcpf.py
+++ challenges/geradorcpf.py
@@ -1,5 +1,24 @@
-from audioop import mul
+from random import randint
 
+numero = str(randint(100000000, 999999999))
+novo_cpf = numero
+reverso = 10
+total = 0
 
-cpf = "46523443841"
-cpf_sem_digito = cpf[:-2]
+for index in range(19):
+    if index > 8:     
+        index -= 9
+
+    total += int(novo_cpf[index]) * reverso
+
+    reverso -= 1    
+    if reverso < 2:
+        reverso = 11
+        d = 11 - (total % 11)
+
+        if d > 9:     
+            d = 0
+        total = 0   
+        novo_cpf += str(d)
+        
+print(novo_cpf)
