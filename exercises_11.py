d5aaa776ac5b853dfce5071fdd7d8140ec241ff1
CaioB99 <caio_b_s@hotmail.com>
2022-04-16 12:31:32 -0300

    Finished functions exercises

--- exercises_11.py
+++ exercises_11.py
@@ -1,9 +1,10 @@
 # -- Desempacotamento
 
-def func(*args):
+def func(*args, **kwargs):
     print(args)
+    print(kwargs)
 
-lista = [1, 2, 3, 4, 5, 6]
-print(*lista, sep="-")
+lista = [1, 2, 3, 4, 5]
+lista2 = [6, 7, 8, 9, 0]
 
-func(1, 2, 3, 4, 5)
+func(*lista, *lista2, nome="Caio", sobrenome="Batista")
