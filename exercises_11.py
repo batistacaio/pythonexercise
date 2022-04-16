58d98ee3f36e8c9abdbe5397c9fc624fbb0ea264
CaioB99 <caio_b_s@hotmail.com>
2022-04-16 12:34:10 -0300

    Finished functions exercises

--- exercises_11.py
+++ exercises_11.py
@@ -1,10 +1,13 @@
-# -- Desempacotamento
+# -- Desempacotamento, args e kwargs
 
 def func(*args, **kwargs):
     print(args)
     print(kwargs)
+    idade = kwargs.get('idade')
+    if idade is not None:
+        print(idade)
 
 lista = [1, 2, 3, 4, 5]
 lista2 = [6, 7, 8, 9, 0]
 
-func(*lista, *lista2, nome="Caio", sobrenome="Batista")
+func(*lista, *lista2, nome="Caio", sobrenome="Batista", idade=25)
