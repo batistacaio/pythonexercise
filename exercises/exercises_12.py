325ecd940a866a552615d310038866b025ca760e
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-04-19 15:14:18 -0300

    Dicts

--- exercises/exercises_12.py
+++ exercises/exercises_12.py
@@ -1,12 +1,19 @@
 d1 = {"Chave1": "Valor_chave1"}
 d1["Chave2"] = "Valor_chave2"
 
-print(d1)
+# - Alterar valor forma 1
 
 d1.update({"Chave1": "Valor_alterado"})
 
-print(d1)
+# - Alterar valor forma 2
 
 d1["Chave2"] = "Valor2_alterado"
 
-print(d1)
+# - Deletar chave
+
+# del d1["Chave2"]
+
+# - Resgatar valores
+
+print("Valor_alterado" in d1.values()) # Retorna true
+
