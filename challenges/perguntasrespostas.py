8b36bf991e7ebe9e5a6a109f0082d662e5f78cbe
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-04-19 16:01:14 -0300

    Lists

---
+++ challenges/perguntasrespostas.py
@@ -0,0 +1,32 @@
+perguntas = {
+    "Pergunta 1": {
+        "pergunta": "Quanto é 2 + 2?",
+        "respostas": {
+          "a": "3",
+          "b": "4",
+          "c": "6",
+          "d": "1"
+        },
+        "resposta_certa": "b"
+    },
+    "Pergunta 2": {
+        "pergunta": "Quanto é 3 * 2?",
+        "respostas": {
+          "a": "2",
+          "b": "10",
+          "c": "6",
+          "d": "0"
+        },
+        "resposta_certa": "c"
+    },
+    "Pergunta 3": {
+        "pergunta": "Quanto é 10 / 5?",
+        "respostas": {
+          "a": "2",
+          "b": "4",
+          "c": "15",
+          "d": "3"
+        },
+        "resposta_certa": "a"
+    },
+}
