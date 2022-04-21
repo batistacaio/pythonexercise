b9af691c0221e5188ea4fdc80111f4c4da5dbdb0
CaioB99 <caio_b_s@hotmail.com>
2022-04-21 14:44:04 -0300

    List comprehension

--- exercises/exercises_14.py
+++ exercises/exercises_14.py
@@ -3,3 +3,10 @@
 l1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 l2 = [v for v in l1]
 print(l2)
+
+multiplicado = [v * 2 for v in l2]
+print(multiplicado)
+
+nomes = ["Caio", "Lucas", "Renato"]
+lnomes = [v.replace("a", "@") for v in nomes]
+print(lnomes)
