017bc48f2fa7bcaee324696d3ad85173d463f653
CaioB99 <caio_b_s@hotmail.com>
2022-04-24 19:39:52 -0300

    Dict comprehension

--- challenges/shopcart.py
+++ challenges/shopcart.py
@@ -4,6 +4,5 @@ carrinho.append(("Produto 1", 50))
 carrinho.append(("Produto 2", 20))
 carrinho.append(("Produto 3", 30))
 
-valor = [preco[1] for preco in carrinho]
-total = sum(valor)
+total = sum([preco[1] for preco in carrinho])
 print(total)
