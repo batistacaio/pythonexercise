cbbf3523c95eec49b6d69fb710ebb6ccd090b17e
CaioB99 <caio_b_s@hotmail.com>
2022-04-24 18:32:38 -0300

    Dict comprehension

--- exercises/exercises_15.py
+++ exercises/exercises_15.py
@@ -7,3 +7,9 @@ lista = [
 
 newdict = {x: y for x, y in lista} # - or dict(lista)
 print(newdict)
+
+upperdict = {x.upper(): y.upper() for x, y in lista}
+print(upperdict)
+
+d1 = {f"chave_{x}": x**2 for x in range(5)}
+print(d1)
