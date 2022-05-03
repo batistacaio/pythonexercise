ba3e35e2ca0e5dcf91ae9f753fd770bc13a62d33
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-03 15:42:00 -0300

    File managing

--- file_manager.py
+++ file_manager.py
@@ -1,8 +1,14 @@
 file = open("text.txt", "w+")
-file.write("Este é um arquivo de teste do Python")
+file.write("Este é um arquivo de teste do Python\n")
+file.write("Esta é uma outra linha")
 
 file.seek(0, 0)
 print("Lendo linha: ")
 print(file.read())
+print("#############")
+
+file.seek(0, 0)
+for linha in file:
+    print(linha, end="")
 
 file.close()
