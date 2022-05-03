ec5cf8d35fd22019779abcd7be641dccf63f42e9
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-03 16:00:55 -0300

    JSON

--- file_manager.py
+++ file_manager.py
@@ -1,8 +1,10 @@
+import json
+
 # w - write mode; r - read mode, a - append mode
 
 file = open("text.txt", "w+")
 file.write("Este é um arquivo de teste do Python\n")
-file.write("Esta é uma outra linha")
+file.write("Esta é uma outra linha\n")
 
 file.seek(0, 0)
 print("Lendo linha: ")
@@ -12,5 +14,20 @@ print("#############")
 file.seek(0, 0)
 for linha in file:
     print(linha, end="")
+print("#############")
 
 file.close()
+
+dict = {
+    "Pessoa 1": {
+        "nome": "Caio",
+        "idade": 25
+    },
+    "Pessoa 2": {
+        "nome": "Lucas",
+        "idade": 33
+    }
+}
+
+dict_json = json.dumps(dict)
+print(dict_json)
