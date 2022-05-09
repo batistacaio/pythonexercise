ea45e811a298bb4cfd08f15042fca45b16d049ff
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-09 13:00:19 -0300

    validacnpj

--- challenges/validacnpj.py
+++ challenges/validacnpj.py
@@ -1,10 +1,35 @@
 import re
 
+REGRESSIVES = [6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2]
+
 def validate(cnpj):
     cnpj = remove_dots(cnpj)
-    print(cnpj)
+    if sequence(cnpj):
+        return False
+    new_cnpj = calc_digit(cnpj=cnpj, digit=1)
 
 def remove_dots(cnpj):
     return re.sub(r'[^0-9]', '', cnpj)
 
-cnpj = validate("33.751.438/0001-00")
+def sequence(cnpj):
+    seq = cnpj[0] * len(cnpj)
+    if seq == cnpj:
+        return True
+    else:
+        return False
+    
+def calc_digit(cnpj, digit):
+    if digit == 1:
+        regressives = REGRESSIVES[1:]
+    elif digit == digit == 2:
+        regressives = REGRESSIVES
+    else:
+        return None
+    
+    total = 0
+    for index, reg in enumerate(regressives):
+        total += int(cnpj[index]) * reg
+    
+    digit1 = 11 - (total % 11)
+    digit1 = digit1 if digit1 <= 9 else 0
+        
