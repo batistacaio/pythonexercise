5902f016bbca1dd3d1b78808f67ec6f16e674ae1
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-10 16:46:27 -0300

    POO

--- challenges/poo/main.py
+++ challenges/poo/main.py
@@ -4,4 +4,8 @@ p1 = Pessoa("Caio", 25)
 
 print(p1.nome)
 p1.comer("abacaxi")
+p1.comer("pastel")
+p1.parar_comer()
 p1.falar("Hoje ainda é terça-feira")
+p1.falar("Hoje não é quinta")
+p1.parar_falar()
--- challenges/poo/pessoa.py
+++ challenges/poo/pessoa.py
@@ -6,9 +6,29 @@ class Pessoa:
         self.falando = falando
         
     def comer(self, alimento):
+        if self.comendo:
+            print(f"{self.nome} já está comendo.")
+            return
         print(f"{self.nome} está comendo {alimento}")
         self.comendo = True
         
+    def parar_comer(self):
+        if not self.comendo:
+            print(f"{self.nome} não está comendo.")
+            return
+        print(f"{self.nome} parou de comer.")
+        self.comendo = False
+        
     def falar(self, frase):
+        if self.falando:
+            print(f"{self.nome} já está falando.")
+            return
         print(f"{self.nome} está falando: '{frase}'")
         self.falando = True
+        
+    def parar_falar(self):
+        if not self.falando:
+            print(f"{self.nome} não está falando.")
+            return
+        print(f"{self.nome} parou de falar.")
+        self.falando = False
