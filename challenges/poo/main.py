88d841516a0c8040ae5d885ba087dd862d361b7c
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-10 16:37:14 -0300

    POO

--- challenges/poo/main.py
+++ challenges/poo/main.py
@@ -3,3 +3,5 @@ from pessoa import Pessoa
 p1 = Pessoa("Caio", 25)
 
 print(p1.nome)
+p1.comer("abacaxi")
+p1.falar("Hoje ainda é terça-feira")
--- challenges/poo/pessoa.py
+++ challenges/poo/pessoa.py
@@ -1,4 +1,16 @@
 class Pessoa:
-    def __init__(self, nome, idade):
+    def __init__(self, nome, idade, comendo=False, falando=False):
         self.nome = nome
         self.idade = idade
+        self.comendo = comendo
+        self.falando = falando
+        
+    def comer(self, alimento):
+        self.alimento = alimento
+        print(f"{self.nome} está comendo {self.alimento}")
+        self.comendo = True
+        
+    def falar(self, frase):
+        self.frase = frase
+        print(f"{self.nome} está falando: '{self.frase}'")
+        self.falando = True
