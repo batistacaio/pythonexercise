9babac87acf76ee94dc6a66aa14c7d739bcc2e53
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-12 16:10:31 -0300

    POO

--- challenges/poo/main.py
+++ challenges/poo/main.py
@@ -1,6 +1,7 @@
 from pessoa import Pessoa
 
 p1 = Pessoa("Caio", 25)
+p2 = Pessoa.por_ano_nascimento("Jessica", 1999)
 
 print(p1.nome)
 p1.comer("abacaxi")
@@ -10,3 +11,4 @@ p1.falar("Hoje ainda é terça-feira")
 p1.falar("Hoje não é quinta")
 p1.parar_falar()
 p1.get_ano_nascimento()
+print(p2.idade)
