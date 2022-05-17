81590d260875a3d2bde0bfc2175adfa16f0d437f
caio_b_s@hotmail.com <caio_b_s@hotmail.com>
2022-05-17 15:42:18 -0300

    POO

--- challenges/poo/main.py
+++ challenges/poo/main.py
@@ -1,5 +1,7 @@
 from pessoa import Pessoa
+from carro import Carro
 
+c1 = Carro("BMW", "Z4", 2019)
 p1 = Pessoa("Caio", 25)
 p2 = Pessoa.por_ano_nascimento("Jessica", 1999)
 
@@ -12,3 +14,4 @@ p1.falar("Hoje não é quinta")
 p1.parar_falar()
 p1.get_ano_nascimento()
 print(p2.idade)
+print(c1.modelo)
